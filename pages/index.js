ed305d5fc11556372c402444b63d731db68f33ee
sameguino <samueleguino97@gmail.com>
2020-10-07 14:11:44 -0400

    adde tile

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Pristine Tech</title>
+      </Head>
       <div className={styles.title} >Pristine Tech</div>
       <div className={styles.description} >Sitio en Construccion</div>
       <img src='under.svg' className={styles.icon} />
